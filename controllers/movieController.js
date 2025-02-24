61a0ac9b326ba12581d5ec0c167fe229563eaa8d
Matteo Cipolla <mati_teo@live.it>
2025-02-24 17:26:15 +0100

    feat: add show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -18,6 +18,52 @@ const index = (req, res) => {
 };
 
 //Show
-const show = (req, res) => {};
+const show = (req, res) => {
+  //recupero l'id dalla rotta
+  const { id } = req.params;
+
+  const movieSql = `
+    SELECT * 
+    FROM movies
+    WHERE id = ?`;
+
+  //lancio la query preparata per leggere il film con id ?
+  connection.execute(movieSql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed ${sql}`,
+      });
+    }
+    //recupero il film dall'array dei risultati
+    const movie = results[0];
+
+    if (!movie) {
+      return res.status(404).json({
+        error: "Not Found",
+        message: "Movie not found",
+      });
+    }
+
+    //query per recuperare le recensioni di quel film
+    const reviewsSql = `
+    SELECT * 
+    FROM reviews
+    WHERE movie_id = ?`;
+
+    connection.execute(reviewsSql, [id], (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Query Error",
+          message: `Database query failed ${reviewsSql}`,
+        });
+      }
+
+      //aggiungo la chiave reviews
+      movie.reviews = results;
+      res.json(movie);
+    });
+  });
+};
 
 module.exports = { index, show };
