858024de2c49a928c389cf1c3252d28b7f38ce84
Matteo Cipolla <mati_teo@live.it>
2025-02-24 16:49:41 +0100

    feat: add db, movies Router and Controllers

---
+++ controllers/movieController.js
@@ -0,0 +1,10 @@
+// Recupero la connessione al database
+const connection = require("../data/db");
+
+//Index
+const index = (req, res) => {};
+
+//Show
+const show = (req, res) => {};
+
+module.exports = { index, show };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const movieController = require("../controllers/movieController");
+
+//La liste delle rotte con la funzione corrispondente del controller
+
+//Index
+router.get("/", movieController.index);
+
+//Show
+router.get("/:id", movieController.show);
 
 module.exports = router;
