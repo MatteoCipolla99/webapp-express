47cdfe7170b82951219274066fe4ccbff4033f27
Matteo Cipolla <mati_teo@live.it>
2025-02-24 16:58:54 +0100

    feat: add index

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,7 +2,20 @@
 const connection = require("../data/db");
 
 //Index
-const index = (req, res) => {};
+const index = (req, res) => {
+  const sql = "SELECT * FROM movies";
+  //lancio la query
+  connection.execute(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed ${sql}`,
+      });
+    }
+
+    res.json(results);
+  });
+};
 
 //Show
 const show = (req, res) => {};
