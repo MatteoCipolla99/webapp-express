1fc2db563e9b45b4c7db2705b50d861d47627030
Matteo Cipolla <mati_teo@live.it>
2025-02-25 17:56:02 +0100

    fix

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const moviesRouter = require("./routers/moviesRouter");
 const notFound = require("./middlewares/notFound");
 const errorsHandler = require("./middlewares/errorsHandler");
+const moviesRouter = require("./routers/moviesRouter");
 
 const app = express();
 const { PORT, FE_URL } = process.env;
-
-// middleware per i file statici
-app.use(express.static("public"));
-
-// middleware per il parsing del req.body
-app.use(express.json());
 
 //middleware CORS
 app.use(
@@ -19,6 +13,12 @@ app.use(
     origin: FE_URL,
   })
 );
+
+// middleware per il parsing del req.body
+app.use(express.json());
+
+// middleware per i file statici
+app.use(express.static("public"));
 
 // Routes (le rotte della mia applicazione)
 app.use("/movies", moviesRouter);
